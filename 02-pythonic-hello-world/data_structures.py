19fe4c33ae763a8594b02d74f1239d62c1b671a0
unknown <adelina.iftode@yahoo.com>
2022-01-19 21:29:00 +0200

    curs02

---
+++ 02-pythonic-hello-world/data_structures.py
@@ -0,0 +1,29 @@
+lista = [8, 2, -5, "string", 2.7]
+# print(type(lista))
+# print(lista[2])
+# print(lista[5])  #Daca indexul nu exista, apare eroare
+# print(lista[1:]) #afisez elementele de la indexul 1 inclusiv, pana la finalul listei
+# print(lista[1:3]) #afisez elementele de la indexul 1 inclusiv, pana la elementul de la indexul 3
+# print(lista[5]) #eroare
+# print(lista[0:5:2]) #elementele de la index 0 pana la indexul 5 (exclux) din 2 in 2
+# denumire_lista[index_inceput : index_final: pas]
+# print(lista[-1]) #ultimul element din lista
+# print(lista[-1::-1]) #afiseaza elementele din lista
+
+# lista.append("mere") #adauga la finalul listei elementul mere
+# lista[3] = "mere" #inlocuieste elementul de la indexul 3(string) cu mere
+# print(lista)
+# print(len(lista)) #afiseaza numarul de elemente din lista
+# print(lista.index(-5))
+# lista.insert(3, "element nou") #adauga un element nou la indexul 3, iar lista se decaleaza
+# lista.remove(-5) # scoate un element din lista, face referile la numele elementului
+# lista.pop(2) #scoate un element din lista, face referire la indexul elementului
+# lista.clear() #goleste lista
+# lista.reverse() #inverseaza ordinea elementelor
+
+# lista_stringuri = ['8', '2', '21']
+# print(ord('0'))
+# print(ord('1'))
+# print(ord('2'))
+# lista_stringuri.sort()
+# print(lista_stringuri)
---
+++ 02-pythonic-hello-world/main.py
@@ -0,0 +1,66 @@
+# print("Numele meu este Adelina")
+# print(1, type(-1)) # tipul int
+# print(1.1, type(1.1)) # type float (rational / zecimal)
+# print(1j, type(1j)) #type complex
+# print(complex(i))
+# print(1+1) #adunare
+# print(1-1) #scadere
+# print(1*3) #inmultire
+# print(5 / 2) #impartire exacta
+# print(5 // 2) #returneaza catul impartirii
+# print(5 % 2) #returneaza restul impartirii (modulo)
+# print(5 ** 2) #ridicarea la putere
+
+# print(5 == 2) #operator de egalitate => returneaza un rezultat boolean (True 1 sau False 0)
+# print(5 != 2) #operator de diferentiere => returneaza un rezultat boolean (True 1 sau False 0)
+
+# print(5 > 2)
+# print(5 < 2)
+# print(5 >= 2)
+# print(5 <= 2)
+
+# print(5>2 and 3>1) #operator logic SI
+# print(5>2 or 3>1) #operator logic SAU
+
+# print(not(5 > 2 and 3 >1)) #operator de negare
+
+# print(7 is 7)
+# print(7 is not 7)
+
+
+# my_var = 0  #asignarea valorii unei variabile
+# my_var, my_var_1 = 0 , 1
+# my_var = 3
+# my_var = my_var + 1 # <=> my_var += 1
+# my_var += 1 # asignare prin adunare / incrementare
+# print(my_var, my_var_1)  #afiseaza valoarea valorii variabilei
+# my_var *= 2 #asignarea prin inmultire
+# print(my_var)
+
+
+# print("Ana are mere")
+# print('Ana are mere')
+
+
+# variabila = "Ana 'are' mere"   # <=> variabila = 'Ana "are" mere'
+
+# variabila = 'Ana \'are\' \n mere'
+
+# variabila = """Ana are
+# mere
+# """
+# print(variabila)
+
+# nume = "Iftode"
+# prenume = "Adelina"
+# varsta = 25
+# inaltime = 1.68
+# prezentare = "Numele meu este {0} si prenumele este {1}".format(nume, prenume)
+# prezentare = f"Numele meu este {nume} si prenumele este {prenume}."
+# prezentare = "Numele meu este" + nume + " si prenumele este " + prenume + "si varsta mea este " + str(varsta) + "."
+# prezentare = "Numele meu este {0} si prenumele este {1} si varsta este {2}".format(nume, prenume, varsta)
+# prezentare = f"Numele meu este {nume} si prenumele este {prenume} si varsta este {varsta}"
+# print(prezentare)
+# calcul = inaltime + varsta
+# calcul = nume + prenume
+# print(calcul)
